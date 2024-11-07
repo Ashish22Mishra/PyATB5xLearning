fa127813db034167bd30efdcc87b53cdf6137b93
Ashish22Mishra <ashish07mishra12@gmail.com>
2024-11-08 04:29:08 +0530

    Git Check

---
+++ Nov/ex_05 Nov'24_Python Basics/Lab-001_Python HelloWorld.py
@@ -0,0 +1,2 @@
+print('Hello World!')
+# This is a comment - Python interpreter will ignore this.
---
+++ Nov/ex_05 Nov'24_Python Basics/Lab-003_Python Basics.py
@@ -0,0 +1,10 @@
+print("Hello world")
+print('Ashish Mishra')
+#       print(2 + 2)  # IndentationError: unexpected indent -> Reformat Code
+# print(self, *objects, sep=' ', end='\n', file=None, flush=False)
+# print(, *args, sep=' ', end='\n', file=None):
+# *args - unlimited number of comma separated arguments
+
+print("Ashish", 28, 'Mishra')
+print("Ashish", 28, 'Mishra', sep="*", end='    ')
+print("Ashish", 28, 'Mishra', sep='@')
